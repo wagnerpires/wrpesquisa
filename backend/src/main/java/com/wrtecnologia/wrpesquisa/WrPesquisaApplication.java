a09f649a3c4b1ae163738c00298f096192371f89
wagnerpires@hotmail.com <wagnerpires@hotmail.com>
2021-06-06 21:27:05 -0300

    Commit Inicial

---
+++ backend/src/main/java/com/wrtecnologia/wrpesquisa/WrPesquisaApplication.java
@@ -0,0 +1,13 @@
+package com.wrtecnologia.wrpesquisa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WrPesquisaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WrPesquisaApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/wrtecnologia/wrpesquisa/WrPesquisaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrtecnologia.wrpesquisa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WrPesquisaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
